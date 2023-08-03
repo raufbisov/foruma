a81408e8ecce7b0ec3ac1cff0ac9bd4d90315bd9
Rauf Abishov <raufbisov@gmail.com>
2023-08-03 15:32:24 +0400

    Set up MessageProducer and tweak application.yml of Interaction

--- interaction/src/main/java/com/raufbisov/interaction/InteractionApplication.java
+++ interaction/src/main/java/com/raufbisov/interaction/InteractionApplication.java
@@ -1,19 +1,20 @@
 package com.raufbisov.interaction;
 
+import com.raufbisov.amqp.RabbitMQMessageProducer;
+import com.raufbisov.auth.AuthClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import com.raufbisov.auth.AuthClient;
-
-@SpringBootApplication
-@EnableFeignClients(
-    basePackageClasses = {
-        AuthClient.class
-    }
+@SpringBootApplication(
+  scanBasePackageClasses = {
+    RabbitMQMessageProducer.class, InteractionConfig.class,
+  }
 )
+@EnableFeignClients(basePackageClasses = { AuthClient.class })
 public class InteractionApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(InteractionApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(InteractionApplication.class, args);
+  }
 }
