a4f5ba07c49dbf187171827b54a0a7a0581d2a8e
Rauf Abishov <raufbisov@gmail.com>
2023-08-03 12:32:56 +0400

    Create RabbitMQ listener factory

--- amqp/src/main/java/com/raufbisov/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/raufbisov/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.raufbisov.amqp;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -21,6 +22,13 @@ public class RabbitMQConfig {
     rabbitTemplate.setMessageConverter(jacksonConverter());
 
     return rabbitTemplate;
+  }
+
+  public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setMessageConverter(jacksonConverter());
+    return factory;
   }
 
   public MessageConverter jacksonConverter() {
